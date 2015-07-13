66083efb738cdc575b7d61cf1beff2702edcd173
rzaari <ryanzaari@gmail.com>
2015-07-13 14:21:21 -0300

    Added contact page to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :contacts
   get '/about' => 'pages#about'
+  get '/contact/new' => 'contacts#new'
 
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
