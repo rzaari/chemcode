90f5f9710417c406cebc2f9b17e657e4d4767dc3
rzaari <ryanzaari@gmail.com>
2015-07-15 10:17:02 -0300

    Added actionmailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['rzaari'],
-  :password => ENV['@monkey2'],
+  :user_name => ENV['xesz135i5429'],
+  :password => ENV['app38637617@heroku.com'],
   :domain => 'heroku.com',
   :enable_startstls_auto => true
 }
