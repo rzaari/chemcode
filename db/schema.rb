dc35d4ef7ff1597324932940e9224d5aee456617
rzaari <ryanzaari@gmail.com>
2015-07-18 14:27:14 -0700

    Added new devise login field username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715232423) do
+ActiveRecord::Schema.define(version: 20150718210705) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150715232423) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
