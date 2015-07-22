516ef0d89639a2560e15f32117c3c89e81a6b716
rzaari <ryanzaari@gmail.com>
2015-07-22 16:05:30 -0700

    Styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
 	def home #we want a home action
     @allposts = Micropost.all
+    @micropost = current_user.microposts.build if current_user
     
 	end
 
