6b0b521e436aa8d440240822a131c0afb51b3457
rzaari <ryanzaari@gmail.com>
2015-07-29 12:38:01 -0700

    Added GAMESS and Molpro seed code

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,10 +6,15 @@ class Micropost < ActiveRecord::Base
 	validates :content, presence: true
 
 	def self.search(search)
-  where("heading LIKE ?", "%#{search}%") 
-  where("content LIKE ?", "%#{search}%")
-  where("software LIKE ?", "%#{search}%") 
-end
+  # where("heading LIKE ?", "%#{search}%") 
+  # where("content LIKE ?", "%#{search}%")
+  # where("software LIKE ?", "%#{search}%")
+	  	if Rails.env.production? 
+			where("heading ILIKE ? OR content ILIKE ? OR software ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+		else
+			where("heading LIKE ? OR content LIKE ? OR software LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+		end
+	end
 
 
 end
