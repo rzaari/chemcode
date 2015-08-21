6c18c93726e1653359d5739c25ff80fc6be81ded
rzaari <ryanzaari@gmail.com>
2015-08-21 14:49:12 -0700

    Add password resets & email configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,7 @@
 class UsersController < ApplicationController
 
-  def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
-  end
-
 	def show
     	@user = User.find(params[:id])
-    	redirect_to root_url and return unless FILL_IN
  	end
 
 	def new
