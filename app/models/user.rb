f46abec7b40c8fac7e5cf9df29690bef81765f6d
rzaari <ryanzaari@gmail.com>
2015-08-21 16:43:05 -0700

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,11 +27,22 @@ class User < ActiveRecord::Base
 	  SecureRandom.urlsafe_base64
 	end
   
+	  # Remembers a user in the database for use in persistent sessions.
+	  def remember
+	    self.remember_token = User.new_token
+	    update_attribute(:remember_digest, User.digest(remember_token))
+	  end
+
     # Returns true if the given token matches the digest.
 	  def authenticated?(attribute, token)
 	    digest = send("#{attribute}_digest")
 	    return false if digest.nil?
 	    BCrypt::Password.new(digest).is_password?(token)
+	  end
+
+	# Forgets a user.
+	  def forget
+	    update_attribute(:remember_digest, nil)
 	  end
 
 	  # Activates an account.
