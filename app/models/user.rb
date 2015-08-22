fe6b8f38c6ad3598a77915c1cbd7788d802cb38f
rzaari <ryanzaari@gmail.com>
2015-08-21 20:03:19 -0700

    Added chapter 9 of Hartl

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ActiveRecord::Base
     # Returns true if the given token matches the digest.
 	  def authenticated?(attribute, token)
 	    digest = send("#{attribute}_digest")
-    	return false if remember_digest.nil?
+    	return false if digest.nil?
 	    BCrypt::Password.new(digest).is_password?(token)
 	  end
 
